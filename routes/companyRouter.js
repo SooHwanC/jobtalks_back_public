5a62e9f9d05a0de8d8130375ca7836df3103ff94
SooHwanC <frcp9408@gmail.com>
2024-06-04 13:21:17 +0900

    0604_인재상저장 추가

--- routes/companyRouter.js
+++ routes/companyRouter.js
@@ -12,4 +12,21 @@ router.get('/companies', async (req, res, next) => {
     }
 });
 
+router.post('/insert', async (req, res, next) => {
+    try {
+        const { company_name, talent } = req.body;
+        logo = 'none';
+        console.log('이름확인', company_name);
+        console.log('인재상 확인', talent);
+        Company.create({ company_name, logo, talent })
+        return res.status(200).send('저장 성공');
+
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+
+
+})
+
 module.exports = router;
